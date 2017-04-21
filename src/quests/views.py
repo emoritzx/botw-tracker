4ac67ba425c3eae4557eb386661c6ffe2fed46f8
Evan Moritz <emoritzx@gmail.com>
2017-04-20 21:42:24 -0400

    First cut at a template

--- src/quests/views.py
+++ src/quests/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Quest
 
 def index(request):
-    return HttpResponse("You are at the quests index.")
+    quest_list = Quest.objects.order_by('quest_name')
+    return render(request, 'quests/index.html', {'quest_list': quest_list})
