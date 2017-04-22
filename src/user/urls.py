f06bdfebaf2d6c9c6fc558df3c0542347d21b2b7
Evan Moritz <emoritzx@gmail.com>
2017-04-22 11:35:57 -0400

    Added url redirects

--- src/user/urls.py
+++ src/user/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^id/(?P<pk>\d+)/$', views.UserProfileView.as_view()),
-    url(r'^(?P<slug>[\w.@+-]+)/$', views.UserProfileView.as_view())
+    url(r'^id/(?P<pk>\d+)/$', views.UserProfileView.as_view(), name='app-views-user-id'),
+    url(r'^(?P<slug>[\w.@+-]+)/$', views.UserProfileView.as_view(), name='app-views-user')
 ]
