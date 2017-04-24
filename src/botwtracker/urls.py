66e99033c269543ad5f6700c8204bd6f4d813f0a
Evan Moritz <emoritzx@gmail.com>
2017-04-23 21:49:11 -0400

    Basic authentication

--- src/botwtracker/urls.py
+++ src/botwtracker/urls.py
@@ -6,6 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
 
 def index(request):
@@ -14,6 +15,8 @@ def index(request):
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^quests/', include('quests.urls')),
     url(r'^user/', include('user.urls')),
 ]
