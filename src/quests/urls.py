e6b3f6c7a3526ca2984fd0809d48d56d1a130508
Evan Moritz <emoritzx@gmail.com>
2017-04-30 13:18:38 -0400

    Added nav links

--- src/quests/urls.py
+++ src/quests/urls.py
@@ -1,6 +1,14 @@
+"""botw-tracker quests app urls
+
+Copyright (c) 2017, Evan Moritz.
+
+botw-tracker is an open source software project released under the MIT License.
+See the accompanying LICENSE file for terms.
+
+"""
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='app-views-quests-index'),
 ]
--- src/user/urls.py
+++ src/user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='app-views-user-index'),
     url(r'^id/(?P<pk>\d+)/$', views.UserProfileView.as_view(), name='app-views-user-id'),
     url(r'^id/(?P<pk>\d+)/update/$', views.UserProfileUpdate.as_view(), name='app-views-user-update'),
     url(r'^(?P<slug>[\w.@+-]+)/$', views.UserProfileView.as_view(), name='app-views-user'),
